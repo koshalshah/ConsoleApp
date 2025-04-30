11bd5c1b98bca2ee31138472fd7b0955c96e6917
koshalshah <koshal01@comcast.net>
2025-04-29 21:43:57 -0500

    Added divide by zero error

--- Program.cs
+++ Program.cs
@@ -33,7 +33,13 @@ class Program
                 break;
 
             case "/":
-                result = num1 / num2;
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                    return;
+                }
                 break;
 
             default:
